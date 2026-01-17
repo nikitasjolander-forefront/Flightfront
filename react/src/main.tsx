e867aad68e0fe03a9f2176076c17e1ea2b2d6ffd
Emmi Quirin <emmi.quirin@forefront.se>
2026-01-17 20:46:14 +0100

    Added Muis ThemeProvider

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.tsx";
+import { ThemeProvider, createTheme } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
