43299b6c8f747081fd88b2d9328bdbea08a11dc0
Emmi Quirin <emmi.quirin@forefront.se>
2026-01-18 16:07:38 +0100

    Added stacking element

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "@mui/material";
 import "./App.css";
 import IcaoInput from "./components/IcaoInput/IcaoInput";
 import WeatherCard from "./components/WatherCard/WeatherCard";
@@ -6,8 +7,10 @@ function App() {
   return (
     <>
       <h1>Flightfront Weather Report</h1>
-      <IcaoInput />
-      <WeatherCard />
+      <Stack spacing={2}>
+        <IcaoInput />
+        <WeatherCard />
+      </Stack>
     </>
   );
 }
