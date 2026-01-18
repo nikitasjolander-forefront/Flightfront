6d50890424d10a3db0681dac13a722fff99baa1e
Emmi Quirin <emmi.quirin@forefront.se>
2026-01-18 16:06:38 +0100

    Added handleSubmit function

--- react/src/components/IcaoInput/IcaoInput.tsx
+++ react/src/components/IcaoInput/IcaoInput.tsx
@@ -1,11 +1,32 @@
 import { Box, Button, TextField } from "@mui/material";
+import type React from "react";
 
 function IcaoInput() {
+  const handleSubmit =(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submited");
+  }
+
+
   return (
     <>
-      <Box component="form">
-        <TextField id="filled-basic" label="required" variant="filled" />
-        <Button  variant="contained" type="submit">Submit</Button>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          alignItems: "strech",
+          gap: 2,
+        }}
+        onSubmit={handleSubmit}
+      >
+        <TextField id="icao-input" label="ICAO" variant="outlined" fullWidth />
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ whiteSpace: "nowrap", minWidth: "150px" }}
+        >
+          Submit
+        </Button>
       </Box>
     </>
   );
