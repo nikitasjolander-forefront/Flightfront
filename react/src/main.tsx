9b9da8c0c81ffe0951a80ef36b590fe70606bc1e
Emmi Quirin <emmi.quirin@forefront.se>
2026-01-19 08:20:38 +0100

    Added queryClientProvider

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.tsx";
 import { ThemeProvider, createTheme } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient;
 
 const theme = createTheme({
   palette: {
@@ -12,8 +14,10 @@ const theme = createTheme({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>,
 );
