2a5060a5a1066d71ffa37fe4a322d1759db8b63a
Nikita Sjölander <nikita.sjolander@sveaskog.se>
2026-01-19 13:38:11 +0100

    Add all info to api response on metar string on icao input

--- dotnet/src/FlightFront.Core/Models/MetarResponse.cs
+++ dotnet/src/FlightFront.Core/Models/MetarResponse.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace FlightFront.Core.Models;
 
 public record MetarResponse(
@@ -7,22 +9,70 @@ public record MetarResponse(
 
 public record MetarData(
     string Icao,
-    string Name,
-    string Raw,
+    [property: JsonPropertyName("raw_text")] string? Raw,
     Barometer? Barometer,
+    Ceiling? Ceiling,
     CloudLayer[]? Clouds,
+    Condition[]? Conditions,
     Temperature? Dewpoint,
-    Temperature? Temperature,
-    string? FlightCategory,
+    Elevation? Elevation,
+    [property: JsonPropertyName("flight_category")] string? FlightCategory,
     Humidity? Humidity,
+    DateTime Observed,
+    Rain? Rain,
+    Station? Station,
+    Temperature? Temperature,
     Visibility? Visibility,
-    Wind? Wind,
-    DateTime Observed
+    Wind? Wind
 );
 
-public record Barometer(double Hg, double Hpa, double Mb);
-public record CloudLayer(int BaseFeetAgl, string Code, string Text);
-public record Temperature(int Celsius, int Fahrenheit);
+public record Barometer(double Hg, double Hpa, double Kpa, double Mb);
+
+public record Ceiling(double Feet, double Meters);
+
+public record CloudLayer(
+    [property: JsonPropertyName("base_feet_agl")] int BaseFeetAgl,
+    [property: JsonPropertyName("base_meters_agl")] int BaseMetersAgl,
+    string Code,
+    string Text,
+    int Feet,
+    int Meters
+);
+
+public record Condition(string Code, string Text);
+
+public record Elevation(double Feet, double Meters);
+
 public record Humidity(int Percent);
-public record Visibility(double Miles, string MilesText, double Meters);
-public record Wind(int Degrees, int SpeedKts, int? GustKts);
+
+public record Rain(double Inches, double Millimeters);
+
+public record Station(
+    Geometry? Geometry,
+    string? Location,
+    string? Name,
+    string? Type
+);
+
+public record Geometry(
+    double[] Coordinates,
+    string Type
+);
+
+public record Temperature(int Celsius, int Fahrenheit);
+
+public record Visibility(
+    double Miles,
+    [property: JsonPropertyName("miles_text")] string? MilesText,
+    double Meters,
+    [property: JsonPropertyName("meters_text")] string? MetersText
+);
+
+public record Wind(
+    int Degrees,
+    [property: JsonPropertyName("speed_kph")] int SpeedKph,
+    [property: JsonPropertyName("speed_kts")] int SpeedKts,
+    [property: JsonPropertyName("speed_mph")] int SpeedMph,
+    [property: JsonPropertyName("speed_mps")] int SpeedMps,
+    [property: JsonPropertyName("gust_kts")] int? GustKts
+);
