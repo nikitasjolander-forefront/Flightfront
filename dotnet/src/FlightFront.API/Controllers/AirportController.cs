4f9d9702e01bea5b397bda0b9b9fce69358f31e4
Nikita Sjölander <nikita.sjolander@sveaskog.se>
2026-01-23 13:21:18 +0100

    add service to  search for icao, airport name and city to get info in api endpoint

---
+++ dotnet/src/FlightFront.API/Controllers/AirportController.cs
@@ -0,0 +1,50 @@
+﻿using FlightFront.Core.Interfaces;
+using FlightFront.Infrastructure.Validators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightFront.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AirportController : ControllerBase
+{
+    private readonly IAirportSearchService _airportSearchService;
+
+    public AirportController(IAirportSearchService airportSearchService)
+    {
+        _airportSearchService = airportSearchService;
+    }
+
+    [HttpGet("searchIcao")]
+    public async Task<IActionResult> SearchIcao([FromQuery] string query, CancellationToken cancellationToken)
+    {
+        var validation = SearchQueryValidator.ValidateQuery(query);
+        if (!validation.IsValid)
+            return BadRequest(validation.ErrorMessage);
+
+        var results = await _airportSearchService.SearchIcaoAsync(query, cancellationToken);
+        return Ok(results);
+    }
+
+    [HttpGet("searchName")]
+    public async Task<IActionResult> SearchName([FromQuery] string query, CancellationToken cancellationToken)
+    {
+        var validation = SearchQueryValidator.ValidateNameQuery(query);
+        if (!validation.IsValid)
+            return BadRequest(validation.ErrorMessage);
+
+        var results = await _airportSearchService.SearchNameAsync(query, cancellationToken);
+        return Ok(results);
+    }
+
+    [HttpGet("searchCity")]
+    public async Task<IActionResult> SearchMuncipality([FromQuery] string query, CancellationToken cancellationToken)
+    {
+        var validation = SearchQueryValidator.ValidateQuery(query);
+        if (!validation.IsValid)
+            return BadRequest(validation.ErrorMessage);
+
+        var results = await _airportSearchService.SearchMuncipalityAsync(query, cancellationToken);
+        return Ok(results);
+    }
+}
---
+++ dotnet/src/FlightFront.Core/Models/Airport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightFront.Core.Models;
+
+public class Airport
+{
+    public string Type { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Continent { get; set; } = string.Empty;
+    public string Municipality { get; set; } = string.Empty;
+    public string IcaoCode { get; set; } = string.Empty;
+}
