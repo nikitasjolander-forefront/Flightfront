1f686b96017be6aaff172c6ad18158c2366d1e2c
Emmi Quirin <emmi.quirin@forefront.se>
2026-01-26 15:39:37 +0100

    refactor: change from useQuery to useMutation for weather fetching

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,15 +1,56 @@
-import { Stack } from "@mui/material";
+import { Box, CircularProgress, Stack, Typography } from "@mui/material";
 import "./App.css";
 import IcaoInput from "./components/IcaoInput/IcaoInput";
-import WeatherCard from "./components/WatherCard/WeatherCard";
+import { WeatherCard } from "./components/WatherCard/WeatherCard";
+import { MetarField } from "./components/MetarField/MetarField";
+// import { useGetWather } from "./hooks/useGetWeather";
+import { useMutation } from "@tanstack/react-query";
+import {
+  getWeatherByIcao,
+  getWeatherByMetar,
+  type Weather,
+} from "./services/weatherServices";
 
 function App() {
+  const weatherMutation = useMutation<
+    Weather,
+    Error,
+    { type: string; value: string }
+  >({
+    mutationFn: async ({ type, value }) => {
+      return type === "icao"
+        ? await getWeatherByIcao(value)
+        : await getWeatherByMetar(value);
+    },
+  });
+
   return (
     <>
       <h1>Flightfront Weather Report</h1>
       <Stack spacing={2}>
-        <IcaoInput />
-        <WeatherCard />
+        <IcaoInput
+          onSubmit={weatherMutation.mutate}
+          icaoRaw={weatherMutation.data ? weatherMutation.data.icao : ""}
+        />
+        <MetarField
+          metarRaw={weatherMutation.data ? weatherMutation.data.metar : ""}
+          onSubmit={weatherMutation.mutate}
+        />
+
+        {weatherMutation.isPending ? (
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            {weatherMutation.error ? (
+              <Typography>Error loading weather..</Typography>
+            ) : null}
+            {weatherMutation.isSuccess ? (
+              <WeatherCard weather={weatherMutation.data} />
+            ) : null}
+          </>
+        )}
       </Stack>
     </>
   );
